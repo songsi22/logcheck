b314d7e3c8508771882dfbb28e2c836db4f0dde8
songsi22 <60905654+songsi22@users.noreply.github.com>
2020-03-04 00:41:53 +0900

    Update logcheck.sh

--- logcheck.sh
+++ logcheck.sh
@@ -1,34 +1,34 @@
-logcheck(){
+beep=0
+cri=0
 curfile=`ls -lt DR_MONTHLY.log.* | head -1|awk '{print $9}'`
 tail -1 $curfile
 wc1=`cat $curfile|wc -l`
 trap trapInt 2
 trap trapQuit 3
-ctrlc_count_quit=0
-ctrlc_count_int=0
-beep=0
+
+
 
 trapQuit()
 {
-    let ctrlc_count_quit++
+	
     if [[ `expr $cri % 2` == 0 ]]; then
 		echo '\033[0;41m'Manual mode'\033[0m'
 		cri=1
-	elif [[ $beep == 1 ]] && [[ `expr $cri % 2` == 1 ]]; then
+		beep=1
+	elif [[ $cri == 1 ]] && [[ $beep == 0 ]]; then
 		echo '\033[0;42m'Automatic mode'\033[0m'
 		beep=0
 		cri=0
+	elif [[ $cri == 1 ]] && [[ $beep == 1 ]]; then
+		echo '\033[0;45m'Automatic mode'\033[0m'
+		cri=0
+		beep=0
 		break
-	else
-		echo '\033[0;42m'Automatic mode'\033[0m'
-		beep=0
-		cri=0
 	fi
 }	
 
 trapInt()
 {
-        let ctrlc_count_int++
     if [[ $beep == 0 ]]; then
 		echo "Do you want to close this program? yes | no ( y | n )"
                 read answer
@@ -37,11 +37,9 @@ trapInt()
                                                 exit
                                                 ;;
                                 no|n)
-												ctrlc_count_int=0
                                                 continue
                                                 ;;
                 esac
-                ctrlc_count_int=0
     elif [[ $beep == 1 ]]; then
 		beep=0
 		break
@@ -50,65 +48,79 @@ trapInt()
 
 while true
 do
-
-        wc2=`cat $curfile|wc -l`
-        if [ $wc1 != $wc2 ]; then
-                line=`tail -1 $curfile`
-                linesc=`echo -e \'$line\'|awk '{print $7}'`
-
-                if [[ "Critical." == $linesc ]] || [[ "Failure" == $linesc ]]; then
-                        cri=1
-                        echo '\033[0;41m\033[7m'$line'\033[0m'
-                        wcc1=`cat $curfile|wc -l`
-                        wcc2=`cat $curfile|wc -l`
-                        while [ $wcc1 == $wcc2 ]
-                        do
-								beep=1
-                                printf '\007'
-                                wcc1=`cat $curfile|wc -l`
-                                sleep 3
-							
-                        done
-
-                elif [ "Warning" == "$linesc" ]; then
-                        if [ $cri == 0 ]; then
-                                echo '\033[0;33m'$line'\033[0m'
-                        else
-                                wcc1=`cat $curfile|wc -l`
-                                wcc2=`cat $curfile|wc -l`
-                                echo '\033[0;36m'$line'\033[0m'
-                                while [ $wcc1 == $wcc2 ]
-                                do
-										beep=1
-                                        printf '\007'
-                                        wcc1=`cat $curfile|wc -l`
-                                        sleep 3
-                                done
-
-                        fi
-                else
-                        if [ $cri == 0 ]; then
-                                echo $line
-                        else
-                                wcc1=`cat $curfile|wc -l`
-                                wcc2=`cat $curfile|wc -l`
-                                echo '\033[0;36m'$line'\033[0m'
-                                while [ $wcc1 == $wcc2 ]
-                                do
-										beep=1
-                                        printf '\007'
-                                        wcc1=`cat $curfile|wc -l`
-                                        sleep 3
-                                done
-                        fi
-
-                fi
+	
+	wc1=`cat $curfile|wc -l`
+	wc2=`cat $curfile|wc -l`
+	if [ $wc1 == $wc2 ]; then
+		while [ $wc1 == $wc2 ]
+		do
+			curfile=`ls -lt DR_MONTHLY.log.* | head -1|awk '{print $9}'`
+			wc1=`cat $curfile|wc -l`
+			sleep 1
+		done
+	fi
+	if [ $wc1 != $wc2 ]; then
+		line=`tail -1 $curfile`
+		linesc=`echo -e \'$line\'|awk '{print $6}'`
+		if [[ "Critical." == $linesc ]] || [[ "Failure" == $linesc ]]; then
+			cri=1
+			beep=1
+			wc2=$wc1
+			echo '\033[0;41m\033[7m'$line'\033[0m'
+			while [ $beep == 1 ]
+			do
+				printf '\007'
+				curfile=`ls -lt DR_MONTHLY.log.* | head -1|awk '{print $9}'`
                 wc1=`cat $curfile|wc -l`
-                sleep 2
-                else
-                curfile=`ls -lt DR_MONTHLY.log.* | head -1|awk '{print $9}'`
-                sleep 2
-        fi
+                sleep 3
+				if [ $wc1 != $wc2 ]; then
+					line=`tail -1 $curfile`
+					echo '\033[0;36m'$line'\033[0m'
+					break
+				fi
+			done
+			
+		elif [ "Warning" == "$linesc" ]; then
+			if [ $cri == 1 ]; then
+			beep=1
+			wc2=$wc1
+			echo '\033[0;36m'$line'\033[0m'
+			while [ $beep == 1 ]
+			do
+				printf '\007'
+                wc1=`cat $curfile|wc -l`
+                sleep 3
+				if [ $wc1 != $wc2 ]; then
+					line=`tail -1 $curfile`
+					echo '\033[0;36m'$line'\033[0m'
+					break
+				fi
+			done
+			else
+				echo '\033[0;33m'$line'\033[0m'
+			fi
+		else
+			if [ $cri == 1 ]; then
+			beep=1
+			wc2=$wc1
+			echo '\033[0;36m'$line'\033[0m'
+			while [ $beep == 1 ]
+			do
+				printf '\007'
+                wc1=`cat $curfile|wc -l`
+                sleep 3
+				if [ $wc1 != $wc2 ]; then
+					line=`tail -1 $curfile`
+					echo '\033[0;36m'$line'\033[0m'
+					break
+				fi
+			done
+			else
+				echo $line
+			fi
+			
+		fi
+		
+	fi
 done
-}
-logcheck 2> /dev/null
+
